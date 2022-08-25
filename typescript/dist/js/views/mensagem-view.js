8fac511c1ce9df9301dd806a652f98df43380735
felipesaev <felipesaev@gmail.com>
2022-08-25 07:28:33 -0300

    chore: add generic type in class

--- typescript/dist/js/views/mensagem-view.js
+++ typescript/dist/js/views/mensagem-view.js
@@ -1,14 +1,8 @@
-export class MensagemView {
-    constructor(selector) {
-        this.element = document.querySelector(selector);
-    }
+import { View } from "./view.js";
+export class MensagemView extends View {
     template(model) {
         return `
         <p class="alert alert-info">${model}</p>
       `;
-    }
-    update(model) {
-        const template = this.template(model);
-        this.element.innerHTML = template;
     }
 }
