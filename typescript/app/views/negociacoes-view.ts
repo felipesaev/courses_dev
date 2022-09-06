3e2d2c87a4d43658099ff2233d7361330ae657e0
felipecosta <felipesaev@gmail.com>
2022-09-06 08:40:45 -0300

    refactor: adjust type method

--- typescript/app/views/negociacoes-view.ts
+++ typescript/app/views/negociacoes-view.ts
@@ -17,7 +17,7 @@ export class NegociacoesView  extends View<Negociacoes> {
           ${model.lista().map(negociacao => {
             return `
               <tr>
-                <td>${new Intl.DateTimeFormat().format(negociacao.data) }</td>
+                <td>${this.formatar(negociacao.data)}</td>
                 <td>${negociacao.quantidade}</td>
                 <td>${negociacao.valor}</td>
               </tr>
@@ -27,4 +27,9 @@ export class NegociacoesView  extends View<Negociacoes> {
       </table>
     `;
   }
+
+  private formatar(data: Date): string {
+    return new Intl.DateTimeFormat().format(data) 
+  }
+
 }
