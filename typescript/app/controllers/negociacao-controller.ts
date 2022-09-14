acea19f832d726e890f330ca956ec077c63e8278
felipecosta <felipesaev@gmail.com>
2022-09-14 08:42:16 -0300

    refactor: adjust method to static

--- typescript/app/controllers/negociacao-controller.ts
+++ typescript/app/controllers/negociacao-controller.ts
@@ -22,7 +22,12 @@ export class NegociacaoController {
     }
 
     public adiciona(): void {
-        const negociacao = this.criaNegociacao();
+
+        const negociacao = Negociacao.criaDe(
+            this.inputData.value,
+            this.inputQuantidade.value,
+            this.inputValor.value
+        );
         if(!this.ehDiaUtil){
             this.mensagemView.update('Apenas negociações em dias úteis são aceitas');
             return;
@@ -30,14 +35,6 @@ export class NegociacaoController {
             this.negociacoes.adiciona(negociacao);        
             this.limparFormulario();
             this.atualizaView();      
-    }
-
-    private criaNegociacao(): Negociacao {
-        const exp = /-/g;
-        const date = new Date(this.inputData.value.replace(exp, ','));
-        const quantidade = parseInt(this.inputQuantidade.value);
-        const valor = parseFloat(this.inputValor.value);
-        return new Negociacao(date, quantidade, valor);
     }
 
     private limparFormulario(): void {
--- typescript/app/models/negociacao.ts
+++ typescript/app/models/negociacao.ts
@@ -13,4 +13,14 @@ export class Negociacao {
         const data = new Date(this._data.getTime());
         return data;
     }
+
+    public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
+        const exp = /-/g;
+        const date = new Date(dataString.replace(exp, ','));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString);
+        return new Negociacao(date, quantidade, valor);
+    }
+
+
 }
